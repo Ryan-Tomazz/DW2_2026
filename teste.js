d504b88a8cd2532849121f260f4ec443e2e42228
alduin10 <ryantomaztalavieira@gmail.com>
2025-03-29 08:50:08 -0300

    Update teste.js

--- teste.js
+++ teste.js
@@ -1,1 +1,12 @@
-let x = "ola, tudo bem. COmo bovê estã? Esta bema?"
+let numero = 16
+let ehprimo = true
+if(numero >0){
+    for(let divisor = 2; divisor < numero; divisor++){
+        if(numero%divisor === 0 ){
+            ehprimo = false;
+            break;
+        }
+    } console.log(ehprimo)
+} else { console.log("ele é um numero negativo, não é possivél ser primo")}
+
+
