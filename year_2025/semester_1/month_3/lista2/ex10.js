127545544ae6dfb6d390e5e6398489013167e6fe
alduin10 <ryantomaztalavieira@gmail.com>
2025-04-04 08:27:09 -0300

    Update ex10.js

--- year_2025/semester_1/month_3/lista2/ex10.js
+++ year_2025/semester_1/month_3/lista2/ex10.js
@@ -1,9 +1,9 @@
 //Crie um código que conte a quantidade de palavras em um textos.
-let x = " vocè é inrifsof, sfosfo, sofso, osfos, fsof, foso, sfos,, sofsof, fosfo,      "
+let x = " vocè é inrifsof, sfosfo, sofso, osfos, fsof, foso, sfos,, sofsof, fosfo,"
 
 function palavras(x){
     let y = x.length
-    let z = x.trim()//porque o trim não funciona
+    let z = x.trim()//porque o trim não funciona?
     let a = z.replaceAll(" ","").length
     let esp = y-a
     let words = esp++
