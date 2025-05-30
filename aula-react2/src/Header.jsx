c0db377ae17fe3f425d17b744886a504d00dfa4b
alduin10 <ryantomaztalavieira@gmail.com>
2025-05-30 11:40:22 -0300

    30

---
+++ aula-react2/src/Header.jsx
@@ -0,0 +1,11 @@
+import './Header.css'
+
+function Header() {
+    return (
+        < header classmane = 'app-header' >
+        <h1>HEREI</h1>
+        </header >
+    )
+}
+
+export default Header
